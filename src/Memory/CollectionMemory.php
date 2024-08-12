5229149f73abc059bb04473822126bd910bd5946
Gregory Lifhits <gregory.lifhits@assaabloy.com>
2024-08-12 08:41:06 -0400

    feat(memory): add asString method to serialize agent messages

--- src/Memory/CollectionMemory.php
+++ src/Memory/CollectionMemory.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace UseTheFork\Synapse\Memory;
 
 use Illuminate\Support\Collection;
+use UseTheFork\Synapse\Integrations\Enums\Role;
 use UseTheFork\Synapse\Integrations\ValueObjects\Message;
 use UseTheFork\Synapse\Memory\Contracts\Memory;
 
@@ -25,6 +26,36 @@ class CollectionMemory implements Memory
     public function get(): array
     {
         return $this->agentMemory->toArray();
+    }
+
+    public function asString(): string
+    {
+        $payload = [];
+        $messages = $this->agentMemory->toArray();
+
+        foreach ($messages as $message) {
+            if ($message['role'] == 'tool') {
+
+                $tool = base64_encode(json_encode([
+                    'name' => $message['tool_name'],
+                    'id' => $message['tool_call_id'],
+                    'arguments' => $message['tool_arguments'],
+                ]));
+
+                $payload[] = "<message type='".Role::ASSISTANT."' tool='{$tool}'></message>";
+
+                $payload[] = "<message type='".Role::TOOL."' tool='{$tool}'>
+         {$message['content']}
+        </message>";
+
+            } else {
+                $payload[] = "<message type='{$message['role']}'>
+         {$message['content']}
+        </message>";
+            }
+        }
+
+        return implode("\n", $payload);
     }
 
     public function load(): void {}
