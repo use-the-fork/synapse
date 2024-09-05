c3f96ae4a0e514923fbd290d23f753d557d4d962
Gregory Lifhits <gregory.lifhits@assaabloy.com>
2024-09-05 09:52:12 -0400

    refactor(tests): relocate and update MultiQueryRetrieverAgent tests for improved structure and mocking

---
+++ tests/Agents/MultiQueryRetrieverAgentTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+  use Saloon\Http\Faking\MockClient;
+  use Saloon\Http\Faking\MockResponse;
+  use Saloon\Http\PendingRequest;
+use UseTheFork\Synapse\Agents\MultiQueryRetrieverAgent;
+use UseTheFork\Synapse\Integrations\Connectors\OpenAI\Requests\ChatRequest;
+
+    it('can run the Multi Query Retriever Agent.', function () {
+
+  MockClient::global([
+                       ChatRequest::class => function (PendingRequest $pendingRequest) {
+                         $count = count($pendingRequest->body()->get('messages'));
+                         return MockResponse::fixture("agents/multi-query-retriever-agent/message-{$count}");
+                       },
+                     ]);
+
+    $agent = new MultiQueryRetrieverAgent();
+
+    $agentResponse = $agent->handle(['queryCount' => '5', 'input' => 'What gym activities do you recommend for heart health?']);
+
+    expect($agentResponse)->toBeArray()
+                          ->and($agentResponse)->toHaveKey('answer')
+                          ->and($agentResponse['answer'])->toBeArray()
+                          ->and($agentResponse['answer'])->toHaveCount(5);
+
+});
