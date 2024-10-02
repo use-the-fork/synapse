af76f6cb221ca58624020c2427785f3096ac7800
use-the-fork <sincore@gmail.com>
2024-10-02 17:27:11 -0400

    refactor(integration): rename OpenAiIntegration to OpenAIIntegration and update methods
    - Renamed `OpenAiIntegration` to `OpenAIIntegration` for naming consistency.
    - Updated method names in `Integration` contract for clarity.
    - Refactored `OpenAIConnector` to remove redundant completion handling methods.
    - Modified `Agent` and `UseIntegration` traits to use the new integration and method names.

---
+++ src/Integrations/OpenAIIntegration.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace UseTheFork\Synapse\Integrations;
+
+use UseTheFork\Synapse\Agent\PendingAgentTask;
+use UseTheFork\Synapse\Contracts\Integration;
+use UseTheFork\Synapse\Integrations\Connectors\OpenAI\OpenAIConnector;
+use UseTheFork\Synapse\Integrations\Connectors\OpenAI\Requests\EmbeddingsRequest;
+use UseTheFork\Synapse\ValueObject\EmbeddingResponse;
+use UseTheFork\Synapse\ValueObject\Message;
+
+class OpenAIIntegration implements Integration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function handlePendingAgentTaskCompletion(
+        PendingAgentTask $pendingAgentTask
+    ): PendingAgentTask {
+
+        $openAIConnector = new OpenAIConnector;
+        $message = $openAIConnector->doCompletionRequest(
+            prompt: $pendingAgentTask->currentIteration()->getPromptChain(),
+            tools: $pendingAgentTask->tools(),
+            extraAgentArgs: $pendingAgentTask->currentIteration()->getExtraAgentArgs()
+        );
+
+        $pendingAgentTask->currentIteration()->setResponse($message);
+
+        return $pendingAgentTask;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function handleCompletion(
+        Message $message,
+        array $extraAgentArgs = []
+    ): Message {
+        $openAIConnector = new OpenAIConnector;
+
+        return $openAIConnector->doCompletionRequest(
+            prompt: [$message],
+            extraAgentArgs: $extraAgentArgs
+        );
+    }
+
+    public function createEmbeddings(string $input, array $extraAgentArgs = []): EmbeddingResponse
+    {
+        return $this->send(new EmbeddingsRequest($input, $extraAgentArgs))->dto();
+    }
+}
